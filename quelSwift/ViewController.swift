98aae5ef60ce80744456755a737dd657592f9d83
Bryan Battu <bryan.battu@etu.univ-amu.fr>
2021-06-18 21:25:44 +0200

    quelSwift

--- quelSwift/ViewController.swift
+++ quelSwift/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var textArray: [String] = [
+        "Appuyez sur un bouton",
+        "Swift est un modèle de voiture",
+        "En anglais, Swift est aussi une espèce d'oiseau",
+        "Hello World!",
+        "Tay Tay"
+    ]
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.layer.cornerRadius = 50
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        imageView.image = UIImage(named: "image (\(sender.tag))")
+        subtitleLabel.text = textArray[sender.tag]
+    }
 }
 
